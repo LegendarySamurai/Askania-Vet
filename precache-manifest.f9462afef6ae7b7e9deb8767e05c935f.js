d5c88df6ed37e6ba534fbceeefab3050d848afd3
Igor Goncharenko <igor.goncharenko86@gmail.com>
2020-02-25 16:16:39 +0200

    Updates

---
+++ precache-manifest.f9462afef6ae7b7e9deb8767e05c935f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64c4e1f0223d3c36a730b5a35a6575e0",
+    "url": "/Askania-Vet/index.html"
+  },
+  {
+    "revision": "641907f61269dd1c8d93",
+    "url": "/Askania-Vet/static/css/2.fe90d041.chunk.css"
+  },
+  {
+    "revision": "92d5c57adc04715e0efd",
+    "url": "/Askania-Vet/static/css/main.bde98498.chunk.css"
+  },
+  {
+    "revision": "641907f61269dd1c8d93",
+    "url": "/Askania-Vet/static/js/2.6ea5cfa0.chunk.js"
+  },
+  {
+    "revision": "e933f8dbfdf496cb72e495663a499a00",
+    "url": "/Askania-Vet/static/js/2.6ea5cfa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92d5c57adc04715e0efd",
+    "url": "/Askania-Vet/static/js/main.eefd99e3.chunk.js"
+  },
+  {
+    "revision": "06fc89b983faed0ca11c",
+    "url": "/Askania-Vet/static/js/runtime-main.15c50c42.js"
+  },
+  {
+    "revision": "34dfeed20d644ba572bd2d8d31bc8d77",
+    "url": "/Askania-Vet/static/media/contemplative-reptile.34dfeed2.jpg"
+  },
+  {
+    "revision": "3890f1a87438ac97f927fd5abf890bed",
+    "url": "/Askania-Vet/static/media/dog-1.3890f1a8.jpg"
+  },
+  {
+    "revision": "f8a799016165cec0a0af4bf577d13c0e",
+    "url": "/Askania-Vet/static/media/dog-2.f8a79901.jpg"
+  },
+  {
+    "revision": "7207f107ad851bd68f82d02eba12f15f",
+    "url": "/Askania-Vet/static/media/dog-3.7207f107.jpg"
+  },
+  {
+    "revision": "90a2c32c66aaacbe1edbca92aab087e7",
+    "url": "/Askania-Vet/static/media/dog-4.90a2c32c.jpg"
+  },
+  {
+    "revision": "f23ec277f2d5dee11810b477f90265f6",
+    "url": "/Askania-Vet/static/media/dog-5.f23ec277.jpg"
+  },
+  {
+    "revision": "786ee2a246e736286d282c0fe5509490",
+    "url": "/Askania-Vet/static/media/logo__askania_trimmed.786ee2a2.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Askania-Vet/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Askania-Vet/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Askania-Vet/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Askania-Vet/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.813302682112f5b992f3ad1107147956.js"
+  "/Askania-Vet/precache-manifest.f9462afef6ae7b7e9deb8767e05c935f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Askania-Vet/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
